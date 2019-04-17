eefe5a20933048fd9fc6e6023a6146d1b19c2859
Seth Pyle <pyl13002@byui.edu>
2019-04-16 19:29:56 -0600

    stuff

--- financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
@@ -1,13 +1,18 @@
 package com.tq.hacknight4.financr.Repo;
 
 import com.plaid.client.*;
+import com.plaid.client.request.TransactionsGetRequest;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PlaidRepo {
     PlaidClient client;
 
+
+
     void test() {
+        client.service().
+        TransactionsGetRequest request = new TransactionsGetRequest()
         client.service().transactionsGet()
     }
 }
