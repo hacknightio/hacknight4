bae3ca20e727187173c42616c0b3ef746f6abe16
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:34:18 -0600

    api token ?

--- financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
@@ -1,9 +1,12 @@
 package com.tq.hacknight4.financr.Config;
 
 import com.plaid.client.PlaidClient;
+import com.plaid.client.request.ItemPublicTokenExchangeRequest;
+import com.plaid.client.response.ItemPublicTokenExchangeResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit2.Response;
 
 @Configuration
 public class PlaidConfig {
@@ -25,5 +28,16 @@ public class PlaidConfig {
                                 .build();
     }
 
+
+    @Bean("api_token")
+    String getToken() {
+      // Synchronously exchange a Link public_token for an API access_token
+      // Required request parameters are always Request object constructor arguments
+      Response<ItemPublicTokenExchangeResponse> response = getClient().service()
+          .itemPublicTokenExchange(new ItemPublicTokenExchangeRequest("the_link_public_token")).execute();
+
+        return response.body().getAccessToken();
+
+    }
 
 }
--- financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
@@ -2,12 +2,20 @@ package com.tq.hacknight4.financr.Repo;
 
 import com.plaid.client.*;
 import com.plaid.client.request.TransactionsGetRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PlaidRepo {
+
+
+    @Autowired
     PlaidClient client;
 
+    @Autowired
+    @Qualifier("api_token")
+    String apiToken;
 
 
     void test() {
