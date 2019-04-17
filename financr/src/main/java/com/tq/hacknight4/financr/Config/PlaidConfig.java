038ee37522bfee066821e8ae1729d790dab75ca8
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:38:06 -0600

    fixed exception error

--- financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
@@ -3,6 +3,9 @@ package com.tq.hacknight4.financr.Config;
 import com.plaid.client.PlaidClient;
 import com.plaid.client.request.ItemPublicTokenExchangeRequest;
 import com.plaid.client.response.ItemPublicTokenExchangeResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,8 @@ import retrofit2.Response;
 
 @Configuration
 public class PlaidConfig {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Value(value="${financr.client_id}")
     String clientId;
@@ -30,7 +35,7 @@ public class PlaidConfig {
 
 
     @Bean("api_token")
-    String getToken() {
+    String getToken() throws IOException {
       // Synchronously exchange a Link public_token for an API access_token
       // Required request parameters are always Request object constructor arguments
       Response<ItemPublicTokenExchangeResponse> response = getClient().service()
--- financr/src/main/java/com/tq/hacknight4/financr/service/SomethingSomethingService.java
+++ financr/src/main/java/com/tq/hacknight4/financr/service/SomethingSomethingService.java
@@ -1,16 +1,22 @@
 package com.tq.hacknight4.financr.service;
 
 import com.tq.hacknight4.financr.Repo.PlaidRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SomethingSomethingService {
 
+  Logger log = LoggerFactory.getLogger(this.getClass());
+
+
   @Autowired
   PlaidRepo plaidRepo;
 
   public void shameMe() {
+    log.info("SERVICE");
 
     plaidRepo.test();
 
