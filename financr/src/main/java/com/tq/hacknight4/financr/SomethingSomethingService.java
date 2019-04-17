2e8cd2936020c16ad97f4b0aac1a0ecd9a340e83
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:16:29 -0600

    loggers, autowires, etc

--- financr/src/main/java/com/tq/hacknight4/financr/SomethingSomethingService.java
+++
@@ -1,8 +0,0 @@
-package com.tq.hacknight4.financr;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SomethingSomethingService {
-
-}
---
+++ financr/src/main/java/com/tq/hacknight4/financr/service/SomethingSomethingService.java
@@ -0,0 +1,15 @@
+package com.tq.hacknight4.financr.service;
+
+import com.tq.hacknight4.financr.Repo.PlaidRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SomethingSomethingService {
+
+  @Autowired
+  PlaidRepo plaidRepo;
+
+
+
+}
