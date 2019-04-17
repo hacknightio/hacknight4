beda43fa92b0073b7eb5ad48b0ac547cf22700c2
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:52:21 -0600

    platypus api token

--- financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
@@ -2,8 +2,12 @@ package com.tq.hacknight4.financr.Config;
 
 import com.plaid.client.PlaidClient;
 import com.plaid.client.request.ItemPublicTokenExchangeRequest;
+import com.plaid.client.request.SandboxPublicTokenCreateRequest;
+import com.plaid.client.request.common.Product;
 import com.plaid.client.response.ItemPublicTokenExchangeResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +17,12 @@ import retrofit2.Response;
 
 @Configuration
 public class PlaidConfig {
+
+    public static final String FIRST_PLATYPUS_BANK_INSTITUTION_ID = "ins_109508";
+    public static final String FIRST_GINGHAM_CREDIT_UNION_INSTITION_ID = "ins_109509";
+    public static final String TATTERSALL_FEDERAL_CREDIT_UNION_INSTITUTION_ID = "ins_109510";
+    public static final String TARTAN_BANK_INSTITUTION_ID = "ins_109511";
+    public static final String HOUNDSTOOTH_BANK_INSTITUTION_ID = "ins_109512";
 
     Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -36,10 +46,20 @@ public class PlaidConfig {
 
     @Bean("api_token")
     String getToken() throws IOException {
+
+    String publicToken =
+        getClient()
+            .service()
+            .sandboxPublicTokenCreate(new SandboxPublicTokenCreateRequest(FIRST_PLATYPUS_BANK_INSTITUTION_ID,
+                Collections.singletonList(Product.TRANSACTIONS)))
+            .execute()
+            .body()
+            .getPublicToken();
+
       // Synchronously exchange a Link public_token for an API access_token
       // Required request parameters are always Request object constructor arguments
       Response<ItemPublicTokenExchangeResponse> response = getClient().service()
-          .itemPublicTokenExchange(new ItemPublicTokenExchangeRequest("the_link_public_token")).execute();
+          .itemPublicTokenExchange(new ItemPublicTokenExchangeRequest(publicToken)).execute();
 
         return response.body().getAccessToken();
 
--- financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
@@ -2,6 +2,8 @@ package com.tq.hacknight4.financr.Repo;
 
 import com.plaid.client.*;
 import com.plaid.client.request.TransactionsGetRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -9,8 +11,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PlaidRepo {
 
+  Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
+
+  @Autowired
     PlaidClient client;
 
     @Autowired
@@ -19,7 +23,9 @@ public class PlaidRepo {
 
 
     public void test() {
-        TransactionsGetRequest request = new TransactionsGetRequest()
-        client.service().transactionsGet()
+
+      log.info("hey-o {}", apiToken);
+//        TransactionsGetRequest request = new TransactionsGetRequest()
+//        client.service().transactionsGet()
     }
 }
