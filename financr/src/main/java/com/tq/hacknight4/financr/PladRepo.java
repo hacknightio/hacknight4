d2f226f1e133eff2b7f40f9f4b3a30a573ef1ca5
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:06:24 -0600

    bbllllaaarg

---
+++ financr/src/main/java/com/tq/hacknight4/financr/PladRepo.java
@@ -0,0 +1,8 @@
+package com.tq.hacknight4.financr;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PladRepo {
+
+}
---
+++ financr/src/main/java/com/tq/hacknight4/financr/ScheduleCtrl.java
@@ -0,0 +1,8 @@
+package com.tq.hacknight4.financr;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ScheduleCtrl {
+
+}
