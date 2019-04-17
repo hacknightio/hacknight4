79e9828e85abb179af5d462bdc86f6fd05eda44f
Dan Langford <danlangford@ldschurch.org>
2019-04-16 19:21:59 -0600

    @repository

--- financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Repo/PlaidRepo.java
@@ -1,7 +1,9 @@
 package com.tq.hacknight4.financr.Repo;
 
 import com.plaid.client.*;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PlaidRepo {
     PlaidClient client;
 
