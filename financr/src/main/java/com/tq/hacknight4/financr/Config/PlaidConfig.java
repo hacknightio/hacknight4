587cfb994e690cc3897d9e41235ddc347e1c2cb9
Seth Pyle <pyl13002@byui.edu>
2019-04-16 19:20:20 -0600

    added config

--- financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
+++ financr/src/main/java/com/tq/hacknight4/financr/Config/PlaidConfig.java
@@ -1,5 +1,29 @@
 package com.tq.hacknight4.financr.Config;
 
+import com.plaid.client.PlaidClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PlaidConfig {
-    
+
+    @Value(value="${financr.client_id}")
+    String clientId;
+
+    @Value(value="${financr.secret}")
+    String secret;
+
+    @Value(value="${financr.public_key}")
+    String publicKey;
+
+    @Bean
+    PlaidClient getClient() {
+        return PlaidClient.newBuilder().clientIdAndSecret(clientId, secret)
+                                .publicKey(publicKey)
+                                .sandboxBaseUrl()
+                                .build();
+    }
+
+
 }
