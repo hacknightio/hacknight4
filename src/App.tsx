5b81dd7f56d4f4e67a82c912a604de8fa18c223c
nate <nathan.borup@gmail.com>
2019-04-16 19:54:34 -0600

    Added input - basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent, FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component <{},{value: number}>{
+  constructor(props:{}) {
+    super(props);
+    this.state = {value: 100};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({value: parseInt(event.target.value)});
+  }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>) {
+    alert('your amount is: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Money Amount:
+          <input type="number" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <label>
+        ${this.state.value}
+      </label>
       </div>
     );
   }
